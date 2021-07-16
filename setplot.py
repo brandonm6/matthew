f273bae08ebdb2a280ea4f8b44d37734b809c3eb
Brandon Maher <brandmaher@gmail.com>
2021-07-16 18:44:57 -0400

    switched method of fetching observational data to requests library (was also a bug with urllib)

--- setplot.py
+++ setplot.py
@@ -13,7 +13,7 @@ import clawpack.geoclaw.data as geodata
 
 import clawpack.geoclaw.surge.plot as surgeplot
 
-import fetchnoaa
+import requests
 
 try:
     from setplotfg import setplotfg
@@ -153,8 +153,119 @@ def setplot(plotdata=None):
                 ('8658120', 'Wilmington, NC')]
 
     landfall_time = np.datetime64('2016-10-08T12:00')
-    begin_date = datetime.datetime(2016, 10, 6)
-    end_date = datetime.datetime(2016, 10, 10)
+    begin_date = datetime.datetime(2016, 10, 6, 12)
+    end_date = datetime.datetime(2016, 10, 9, 12)
+
+    def fetch_noaa_tide_data(station, begin_date, end_date, time_zone='GMT',
+                             datum='STND', units='metric', cache_dir=None,
+                             verbose=True):
+
+        begin_date = begin_date.strftime('%Y%m%d %H:%M')
+        end_date = end_date.strftime('%Y%m%d %H:%M')
+
+        noaa_api_url = 'https://tidesandcurrents.noaa.gov/api/datagetter'
+
+        # use geoclaw scratch directory for caching by default
+        if cache_dir is None:
+            if 'CLAW' not in os.environ:
+                raise ValueError('CLAW environment variable not set')
+            claw_dir = os.environ['CLAW']
+            cache_dir = os.path.join(claw_dir, 'geoclaw', 'scratch')
+
+        def fetch(product, expected_header, col_idx, col_types):
+            noaa_params = get_noaa_params(product)
+            cache_path = get_cache_path(product)
+
+            # use cached data if available
+            if os.path.exists(cache_path):
+                if verbose:
+                    print('Using cached {} data for station {}'.format(
+                        product, station))
+                return parse(cache_path, col_idx, col_types, header=True)
+
+            # otherwise, retrieve data from NOAA and cache it
+            if verbose:
+                print('Fetching {} data from NOAA for station {}'.format(
+                    product, station))
+            response = requests.get(noaa_api_url, noaa_params)
+            text = response.text
+            # ensure that received header is correct
+            header = text.splitlines()[0].strip()
+            if header != expected_header or not response:
+                # response contains error message
+                raise ValueError("text")
+            # if there were no errors, then cache response
+            save_to_cache(cache_path, text)
+
+            return parse(cache_path, col_idx, col_types, header=True)
+
+        def get_noaa_params(product):
+            noaa_params = {
+                'product': product,
+                'application': 'NOS.COOPS.TAC.WL',
+                'format': 'csv',
+                'station': station,
+                'begin_date': begin_date,
+                'end_date': end_date,
+                'time_zone': time_zone,
+                'datum': datum,
+                'units': units
+            }
+            return noaa_params
+
+        def get_cache_path(product):
+            dates = '{}_{}'.format(begin_date, end_date)
+            filename = '{}_{}_{}'.format(time_zone, datum, units)
+            abs_cache_dir = os.path.abspath(cache_dir)
+
+            return os.path.join(abs_cache_dir, product, station, dates, filename)
+
+        def save_to_cache(cache_path, data):
+            # make parent directories if they do not exist
+            parent_dir = os.path.dirname(cache_path)
+            if not os.path.exists(parent_dir):
+                os.makedirs(parent_dir)
+
+            # write data to cache file
+            with open(cache_path, 'w') as cache_file:
+                cache_file.write(data)
+
+        def parse(data, col_idx, col_types, header):
+            # read data into structured array, skipping header row if present
+            a = np.genfromtxt(data, usecols=col_idx, dtype=col_types,
+                              skip_header=int(header), delimiter=',',
+                              missing_values='')
+
+            # return tuple of columns
+            return tuple(a[col] for col in a.dtype.names)
+
+        # only need first two columns of data; first column contains date/time,
+        # and second column contains corresponding value
+        col_idx = (0, 1)
+        col_types = 'datetime64[m], float'
+
+        # fetch water levels and tide predictions
+        date_time, water_level = fetch(
+            'water_level', 'Date Time, Water Level, Sigma, O or I (for verified), F, R, L, Quality',
+            col_idx, col_types)
+        date_time2, prediction = fetch('predictions', 'Date Time, Prediction',
+                                       col_idx, col_types)
+
+        # ensure that date/time ranges are the same
+        if not np.array_equal(date_time, date_time2):
+            raise ValueError('Received data for different times')
+
+        return date_time, water_level, prediction
+
+    def get_actual_water_levels(station_id, begin_date, end_date, landfall_time):
+        # Fetch water levels and tide predictions for given station
+        date_time, water_level, tide = fetch_noaa_tide_data(station_id, begin_date, end_date, datum='NAVD')
+        # Subtract tide predictions from measured water levels
+        water_level -= tide
+        # Calculate times relative to landfall
+        seconds_rel_landfall = (date_time - landfall_time) / np.timedelta64(1, 's')
+
+        return seconds_rel_landfall, water_level
 
     def gauge_afteraxes(current_data):
         axes = plt.gca()
@@ -162,8 +273,8 @@ def setplot(plotdata=None):
         surgeplot.plot_landfall_gauge(current_data.gaugesoln, axes)
         station_id, station_name = stations[current_data.gaugeno - 1]
 
-        seconds_rel_landfall, actual_level = fetchnoaa.get_actual_water_levels(station_id, begin_date, end_date,
-                                                                               landfall_time)
+        seconds_rel_landfall, actual_level = get_actual_water_levels(station_id, begin_date, end_date,
+                                                                     landfall_time)
         axes.plot(seconds_rel_landfall, actual_level, 'g', label='Observed')
 
         # Fix up plot - in particular fix time labels
